18166e84a45c33ae593850cc5a68397040deb83a
Oleg Kravtcov <71754272+MaestrOle@users.noreply.github.com>
2020-11-26 17:10:10 +0200

    Add files via upload

---
+++ DAL/Interfaces/ICustomerDAL.cs
@@ -0,0 +1,10 @@
+﻿using DTO;
+
+namespace DAL.Interfaces
+{
+    public interface ICustomerDal
+    {
+        CustomerDTO CreateCustomer(string FirstName, string LastName, int YearOfBirth, string email, string password);
+        bool Login(string email, string password);
+    }
+}
---
+++ DAL/Interfaces/IGamesDAL.cs
@@ -0,0 +1,13 @@
+﻿using DTO;
+using System.Collections.Generic;
+
+namespace DAL.Interfaces
+{
+    public interface IGamesDal
+    {
+        List<GamesDTO> GamesInGenre(int genreID);
+        List<GamesDTO> Sort(string Title, int genreID);
+        List<GamesDTO> Find(string Title, int genreID);
+        GamesDTO ShowGameInfo(int gameID);
+    }
+}
