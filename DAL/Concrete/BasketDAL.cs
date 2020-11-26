0bb0a4ffd2c0998a41e30c4f05a3bd60dc34c3e2
Oleg Kravtcov <71754272+MaestrOle@users.noreply.github.com>
2020-11-26 17:06:20 +0200

    Add files via upload

---
+++ DAL/Concrete/BasketDAL.cs
@@ -0,0 +1,32 @@
+﻿using DAL.Interfaces;
+using System;
+using System.Data.SqlClient;
+using DTO;
+
+namespace DAL.Concrete
+{
+    public class BasketDal : IBasketDal
+    {
+        private string connectionString;
+
+        public BasketDal(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void AddToBasket(BasketDTO newelem)
+        {
+            using (SqlConnection conn = new SqlConnection(this.connectionString))
+            using (SqlCommand comm = conn.CreateCommand())
+            {
+                comm.CommandText = "insert into Basket (Title, Price) output INSERTED.BasketID values (@Title, @Price)";
+                comm.Parameters.Clear();
+                comm.Parameters.AddWithValue("@Title", newelem.Title);
+                comm.Parameters.AddWithValue("@Price", newelem.Price);
+                conn.Open();
+
+                newelem.BasketID = Convert.ToInt32(comm.ExecuteScalar());
+            }
+        }
+    }
+}
---
+++ DAL/Concrete/GenresDAL.cs
@@ -0,0 +1,42 @@
+﻿using DTO;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+namespace DAL.Concrete
+{
+    public class GenresDal : IGenreDal
+    {
+        private string connectionString;
+
+        public GenresDal(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        string connStr = ConfigurationManager.ConnectionStrings["GamesDB"].ConnectionString;
+        public List<GenresDTO> GetAllGenres()
+        {
+            using (SqlConnection conn = new SqlConnection(this.connStr))
+            using (SqlCommand comm = conn.CreateCommand())
+            {
+                comm.CommandText = "select * from Genres";
+                conn.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+
+                List<GenresDTO> genres = new List<GenresDTO>();
+                while (reader.Read())
+                {
+
+                    genres.Add(new GenresDTO
+                    {
+                        GenreID = (int)reader["GenreID"],
+                        GenreName = reader["GenreName"].ToString(),
+
+                    });
+
+                }
+                return genres;
+            }
+        }
+    }
+
+}
