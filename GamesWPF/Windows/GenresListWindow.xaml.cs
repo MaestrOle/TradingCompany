d44df57522ff1ac2d79bba5ef96721d45ada65a2
Oleg Kravtcov <71754272+MaestrOle@users.noreply.github.com>
2020-12-03 01:04:26 +0200

    Add files via upload

---
+++ GamesWPF/Windows/GenresListWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using GamesWPF.ViewModels;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using Unity;
+
+
+namespace GamesWPF.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для GenresListWindow.xaml
+    /// </summary>
+    public partial class GenresListWindow : Window
+    {
+        GenresListModel _genreListViewModel;
+        CollectionViewSource _GenresList;
+        public GenresListWindow(GenresListModel vm)
+        {
+            InitializeComponent();
+            _genreListViewModel = vm;
+            DataContext = vm;
+
+            _GenresList = (CollectionViewSource)(Resources["GenresCollection"]);
+        }
+        private void DgGenres_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var gamesInGenre = ((App)Application.Current).Container.Resolve<GamesInGenre>();
+            gamesInGenre.ShowDialog();
+        }
+        private void Logout_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
